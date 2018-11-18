0d887316430162858b82c276d9cd6254dea91372
wellingtonapolonio <wellingtonpoloniosilva@gmail.com>
2018-11-17 23:07:48 -0200

    first commit

---
+++ src/application/Exe2.java
@@ -0,0 +1,70 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import entities.Employee;
+
+public class Exe2 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			List<Employee>list = new ArrayList<>();
+			
+
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				
+				list.add(new Employee(fields[0],fields[1], Double.parseDouble(fields[2])));
+				line = br.readLine();
+			}
+			
+			System.out.print("Enter salary:");
+			double sal = sc.nextDouble();
+			
+			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+			
+			
+			List<String> e = list.stream().filter(x -> x.getSalary() < sal).
+					map(p -> p.getEmail()).sorted(comp.reversed()).collect(Collectors.toList());
+			
+			
+			
+			System.out.println("Email of people whose salary is more than " + String.format("%.2f", sal)+ ":");
+			e.forEach(System.out:: println);
+			
+			double sum = list.stream().filter(p -> p.getName().charAt(0) =='M')
+					.map(x -> x.getSalary()).reduce(0.0,(x,y)-> x + y * 1.1);
+			
+			System.out.println("Sum of salary of people whose name starts with 'M': " + String.format("%.2f", sum));
+			
+			
+			
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		sc.close();
+	}
+
+
+
+	}
+
+
